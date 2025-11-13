ae1a6dae08e3d9ed09b73b8fe1ffa22e6f2f3dbf
student <kopmax2008@yandex.ru>
2025-11-13 12:15:40 +0300

    Created

--- src/main/java/org/keyart/test/registry/ModBlocks.java
+++ src/main/java/org/keyart/test/registry/ModBlocks.java
@@ -3,7 +3,6 @@ package org.keyart.test.registry;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -12,6 +11,7 @@ import org.keyart.test.Test;
 import org.keyart.test.items.BlockOfKakashka;
 
 import java.util.function.Supplier;
+
 
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Test.MODID);
